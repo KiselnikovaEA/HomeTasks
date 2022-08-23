ccb19606cdbba4f67909a455c563996459a8425d
KisEA <maslennikvakat@mail.ru>
2022-08-23 18:16:37 +0300

    Initial commit - добавлено решение первой задачи и .gitignore

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("Введите первое целое число: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе целое число: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("Наибольшее число " + num1);
+            }
+            else
+            {
+                Console.WriteLine("Наибольшее число " + num2);
+            }
+
+        }
+    }
+}
