70a21abdaff0b3e7e7b072f4cea2a90dcce7bf18
KisEA <maslennikvakat@mail.ru>
2022-08-25 01:01:37 +0300

    Добавлено решение третьей задачи

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("Введите целое число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }
+
+        }
+    }
+}
