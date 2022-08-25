c01de854c9671115f062f8f6129509af00105706
KisEA <maslennikvakat@mail.ru>
2022-08-25 16:40:48 +0300

    Добавлено решение четвертой задачи

---
+++ Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("Введите неотрицательное целое число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int i = 2;
+
+            if(num >= i)
+            {
+            Console.Write("Все четные числа от 1 до " + num + ": ");
+
+                while(i <= num)
+                {
+                    Console.Write(i + " ");
+                    i = i + 2;
+                }
+
+            }
+            else
+            {
+                Console.Write("Введите число побольше");
+            }
+
+        }
+    }
+}
